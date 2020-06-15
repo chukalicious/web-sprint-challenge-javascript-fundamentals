78ec3c74da4bc95f8b50724db3146579f68f7a2a
Katiuska <katiuskaalicea@gmail.com>
2020-06-14 20:14:44 -0700

    attempted 2nd closures exercise

--- challenges/arrays-callbacks.js
+++ challenges/arrays-callbacks.js
@@ -47,7 +47,8 @@ console.log(lowPopulationAnimals);
 The zoos need to know their total animal population across the United States. Find the total population from all the zoos using the .reduce() method. Remember the reduce method takes two arguments: a callback (which itself takes two args), and an initial value for the count.
 
 */
-let populationTotal = zooAnimals.population.reduce((num, sum) => num + sum); 
+let populationTotal = 0; 
+zooAnimals.population.reduce((num, sum) => num + sum); 
 console.log(populationTotal);
 
 
--- challenges/closure.js
+++ challenges/closure.js
@@ -17,8 +17,19 @@ function myFunction() {
 myFunction();
 
 // Explanation: 
-
+//Because that's how it works: inner scopes can reach out for information in outer scopes but outer scpes cannot reach into inner scopes to get information (variables and functions)
 
 /* Task 2: Counter */
 
 /* Create a function called `summation` that accepts a parameter and uses a counter to return the summation of that number. For example, `summation(4)` should return 10 because 1+2+3+4 is 10. */
+
+function summation(num) {
+  let sum = 0; 
+  for (var i = 0; i < num; i++) {
+    sum ++; 
+  }
+   
+  num = sum;
+  return sum; 
+}
+console.log(summation(4))
