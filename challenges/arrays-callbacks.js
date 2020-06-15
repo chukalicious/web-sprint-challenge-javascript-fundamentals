fe245ff46369272e56bc324a5e846d96c9808194
Katiuska <katiuskaalicea@gmail.com>
2020-06-14 19:14:14 -0700

    completed step 1 of callbacks

--- challenges/arrays-callbacks.js
+++ challenges/arrays-callbacks.js
@@ -47,7 +47,7 @@ console.log(lowPopulationAnimals);
 The zoos need to know their total animal population across the United States. Find the total population from all the zoos using the .reduce() method. Remember the reduce method takes two arguments: a callback (which itself takes two args), and an initial value for the count.
 
 */
-let populationTotal = 0;
+let populationTotal = zooAnimals.population.reduce((num, sum) => num + sum); 
 console.log(populationTotal);
 
 
@@ -60,6 +60,9 @@ console.log(populationTotal);
   * The consume function should return the invocation of cb, passing a and b into cb as arguments
 */
 
+function consume(a, b, cb) {
+  return cb(a, b)
+}
 
 /* Step 2: Create several functions to callback with consume();
   * Create a function named add that returns the sum of two numbers
