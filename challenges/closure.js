f95549dec9aebb89b49c6b7869b4fdd44bd62103
Katiuska <katiuskaalicea@gmail.com>
2020-06-14 20:53:15 -0700

    finished closures, Natalia confirmed what was requested

--- challenges/closure.js
+++ challenges/closure.js
@@ -23,13 +23,13 @@ myFunction();
 
 /* Create a function called `summation` that accepts a parameter and uses a counter to return the summation of that number. For example, `summation(4)` should return 10 because 1+2+3+4 is 10. */
 
+//I was confused about the instructions because the example shows a reduce-y kind of result but Natalia (my Sunday TL) confirmed that what's on my code is what's requested
+
 function summation(num) {
   let sum = 0; 
   for (var i = 0; i < num; i++) {
     sum ++; 
   }
-   
-  num = sum;
   return sum; 
 }
 console.log(summation(4))
