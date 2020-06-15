761e7890804dd803711f96fbdfb26fb2db8abfcb
Katiuska <katiuskaalicea@gmail.com>
2020-06-14 21:57:24 -0700

    last commit for tonight

--- challenges/arrays-callbacks.js
+++ challenges/arrays-callbacks.js
@@ -63,7 +63,7 @@ console.log(populationTotal);
   * The consume function should return the invocation of cb, passing a and b into cb as arguments
 */
 
-function consume(a, b, cb) { //this works when I enter it directly on the console
+function consume(a, b, cb) { 
   return cb(a, b)
 }
 
@@ -73,23 +73,22 @@ function consume(a, b, cb) { //this works when I enter it directly on the consol
   * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
 */
 
-function add(a, b) {//this works when I enter it directly on the console
+function add(a, b) {
   return a + b
 }
 
-function multiply(a, b) {//this works when I enter it directly on the console
+function multiply(a, b) {
   return a * b
 }
 
-function greeting (firstName, lastName) {//this works when I enter it directly on the console
+function greeting (firstName, lastName) {
   return `Hello ${firstName} ${lastName}, nice to meet you`
 }
+
 /* Step 3: Check your work by un-commenting the following calls to consume(): */
 
 console.log(consume(2, 2, add)); // 4
-
 console.log(consume(10, 16, multiply)); // 160
-
 console.log(consume("Mary", "Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
 
 
